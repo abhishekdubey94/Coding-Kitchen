e0d38f1fed7a444196fcf2e9c9812844e0e225fd
abhishekdubey94 <abhishekdubey94@gmail.com>
2020-11-18 08:28:51 +0530

    permutation

---
+++ DynamicProgramming/382_PermutationCoefficient/sol.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int permutationCoeff(int n,int k){
+    int dp[n+1][k+1];
+
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=min(i,k);j++){
+            if(j==0){
+                dp[i][j] = 1;
+            }else{
+                dp[i][j] = dp[i-1][j] + j*dp[i-1][j-1];
+            }
+            dp[i][j + 1] = 0; 
+        }
+    }
+    return dp[n][k];
+}
+
+int main(){
+    int n,k;
+    cin>>n>>k;
+    cout<<"Value of P("<<n<<","<<k<<") is "<<permutationCoeff(n,k);
+    return 0;
+}
